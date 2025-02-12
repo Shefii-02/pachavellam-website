cae84f3a9ee9e3f93065bba46eae645c02ef8dda
Shefii-02 <shefii.km@gmail.com>
2025-02-12 23:04:34 +0530

    role page

--- resources/views/admin/roles/edit.blade.php
+++ resources/views/admin/roles/edit.blade.php
@@ -1,70 +1,73 @@
 @extends('admin.general.layout')
-    @section('general')
-        <div class="row">
-            <div class="col-lg-12 row">
-                <div class="col-md-6 text-left">
-                    <div class="col-12 mb-3">
-                        <h1 class="h3 mb-2 mb-sm-0">Edit Role</h1>
-                    </div>
-                </div>
-                <div class="col-md-6 text-end">
-                    <a class="btn btn-primary" href="{{ route('admingeneralrole.index') }}"><i class="fas fa-arrow-left"></i> Back</a>
+
+@section('general')
+    <div class="row">
+        <div class="col-lg-12 row">
+            <div class="col-md-6 text-left">
+                <div class="col-12 mb-3">
+                    <h1 class="h3 mb-2 mb-sm-0">Edit Role</h1>
                 </div>
             </div>
+            <div class="col-md-6 text-end">
+                <a class="btn btn-primary" href="{{ route('admingeneralrole.index') }}">
+                    <i class="fas fa-arrow-left"></i> Back
+                </a>
+            </div>
         </div>
+    </div>
 
-
-    @if (count($errors) > 0)
+    @if ($errors->any())
         <div class="alert alert-danger">
             <strong>Whoops!</strong> There were some problems with your input.<br><br>
             <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
             </ul>
         </div>
     @endif
 
+    <form action="{{ route('admingeneralrole.update', $role->id) }}" method="POST">
+        @csrf
+        @method('PATCH')
 
-{!! Form::model($role, ['method' => 'PATCH','route' => ['admingeneralrole.update', $role->id]]) !!}
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group mb-3">
-            <strong>Name:</strong>
-            {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group mb-3">
-            <strong class="mb-3">Permission:</strong>
-            <br/><input type="checkbox" id="checkAll">Check All
-            <div class="row">
-
-                @foreach($permission as $value)
-                <div class="col-6">
-                    <label>{{ Form::checkbox('permission[]', $value->id, in_array($value->id, $rolePermissions) ? true : false, array('class' => 'name')) }}
-                    {{ $value->name }}</label>
-
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group mb-3">
+                    <strong>Name:</strong>
+                    <input type="text" name="name" value="{{ old('name', $role->name) }}" placeholder="Name" class="form-control">
                 </div>
-            
-                @endforeach
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group mb-3">
+                    <strong class="mb-3">Permission:</strong>
+                    <br/><input type="checkbox" id="checkAll"> Check All
+                    <div class="row">
+                        @foreach($permission as $value)
+                            <div class="col-6">
+                                <label>
+                                    <input type="checkbox" name="permission[]" value="{{ $value->id }}"
+                                        {{ in_array($value->id, $rolePermissions) ? 'checked' : '' }} class="name">
+                                    {{ $value->name }}
+                                </label>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-left">
+                <button type="submit" class="btn btn-primary">
+                    <i class="fas fa-paper-plane"></i> Update
+                </button>
             </div>
         </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12 text-left">
-        <button type="submit" class="btn btn-primary"><i class="fas fa-paper-plane"></i> Update</button>
-    </div>
-</div>
-{!! Form::close() !!}
-
-
+    </form>
 @endsection
 
 @section('scripts')
 <script>
-     $("#checkAll").click(function () {
-         $('input:checkbox').not(this).prop('checked', this.checked);
-     });
+    $("#checkAll").click(function () {
+        $('input:checkbox').not(this).prop('checked', this.checked);
+    });
 </script>
-
 @endsection
--- resources/views/admin/roles/show.blade.php
+++ resources/views/admin/roles/show.blade.php
@@ -1,35 +1,36 @@
 @extends('admin.general.layout')
-    @section('general')
-    <div class="row">
-        <div class="col-lg-12 row">
-            <div class="col-md-6 text-left">
-                <div class="col-12 mb-3">
-                    <h1 class="h3 mb-2 mb-sm-0">Show  Role</h1>
+
+@section('general')
+    <div class="row mb-4">
+        <div class="col-lg-12 d-flex justify-content-between align-items-center">
+            <h1 class="h3 mb-0">Show Role</h1>
+            <a class="btn btn-primary" href="{{ route('admingeneralrole.index') }}">
+                <i class="fas fa-arrow-left"></i> Back
+            </a>
+        </div>
+    </div>
+
+    <div class="card shadow-lg border-0 rounded">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-12 mb-4">
+                    <h5 class="text-primary"><i class="fas fa-user-tag"></i> Role Name</h5>
+                    <div class="alert alert-light border p-2">{{ $role->name }}</div>
+                </div>
+
+                <div class="col-md-12">
+                    <h5 class="text-primary"><i class="fas fa-shield-alt"></i> Permissions</h5>
+                    <div class="border p-3 rounded bg-light">
+                        @if(!empty($rolePermissions))
+                            @foreach($rolePermissions as $v)
+                                <span class="badge bg-success px-3 py-2 me-1 mb-1">{{ $v->name }}</span>
+                            @endforeach
+                        @else
+                            <span class="text-muted">No permissions assigned</span>
+                        @endif
+                    </div>
                 </div>
             </div>
-            <div class="col-md-6 text-end">
-                <a class="btn btn-primary" href="{{ route('admingeneralrole.index') }}"><i class="fas fa-arrow-left"></i> Back</a>
-            </div>
         </div>
     </div>
-
-
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group mb-3">
-            <strong>Name:</strong><br>
-            {{ $role->name }}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group mb-3">
-            <strong class="mb-3">Permissions:</strong><br>
-            @if(!empty($rolePermissions))
-                @foreach($rolePermissions as $v)
-                    <label class="label label-success">{{ $v->name }},</label>
-                @endforeach
-            @endif
-        </div>
-    </div>
-</div>
 @endsection
