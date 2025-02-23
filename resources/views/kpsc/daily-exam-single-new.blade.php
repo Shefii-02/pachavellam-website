4cb0ed521e020ef55bda0ea2f254dbba7f0ac160
Shefii-02 <shefii.km@gmail.com>
2025-02-23 20:03:09 +0530

    ttt

--- resources/views/kpsc/daily-exam-single-new.blade.php
+++ resources/views/kpsc/daily-exam-single-new.blade.php
@@ -70,7 +70,7 @@
 
                     <div class="optionContainer">
                         @foreach($question['responses'] as $respIndex => $response)
-                            <div class="option " onclick="selectOption({{ $index }}, {{ $respIndex }})">
+                            <div class="option " style="    display: flex;gap: 9px;" onclick="selectOption({{ $index }}, {{ $respIndex }})">
                                 {!! chr(97 + $respIndex) !!}. {!! $response['text'] !!}
                             </div>
                         @endforeach
