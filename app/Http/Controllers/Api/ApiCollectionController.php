01366dbe5d8ab9aded09b375e0b9983ecd372c4d
Shefii-02 <shefii.km@gmail.com>
2025-03-27 13:58:13 +0530

    corrected

--- app/Http/Controllers/Api/ApiCollectionController.php
+++ app/Http/Controllers/Api/ApiCollectionController.php
@@ -302,6 +302,6 @@ class ApiCollectionController extends Controller
 
     public function CADailyExamLeaderboard(Request $request) {
         $exam_attended = CaDailyExamAttempt::with('user')->where('exam_id', $request->exam_id)->orderBy('total','desc')->orderBy('created_at','asc')->get();
-        return response()->json(['data' => CADailyExamLeaderboardResources::collection($exam_attended), 'status' => 200]);
+        return response()->json(['data' => CADailyExamLeaderboardResources::collection($exam_attended),'currentRank'=>2, 'status' => 200]);
     }
 }
